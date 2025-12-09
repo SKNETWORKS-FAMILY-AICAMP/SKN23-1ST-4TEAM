a1941177d345aa2c440abaa8f9ca371233eab1e8
JUYEOP024 <juyeobsong63@gmail.com>
2025-12-10 08:55:38 +0900

    함수 추가

---
+++ backend/db_main/common_repository.py
@@ -0,0 +1,75 @@
+from utils.db_utils import fetch_all_dict
+
+def get_regions():
+    """
+    C001 - 시도 목록 조회
+    Returns: list(dict) -> [{} sido_name}, ...]
+    """
+    query = """
+        SELECT
+            sido_name
+        FROM dim_region_sido;
+    """
+
+    rows = fetch_all_dict(query)
+
+    return [
+        {
+            "sido_name": row["sido_name"]
+        }
+        for row in rows
+    ]
+
+"""------------------------------------------------"""
+
+
+from utils.db_utils import fetch_all_dict
+
+def get_fuel_types():
+    """
+    D101 - 연료 기준정보 조회
+    Returns: list(dict) — fuel_id, fuel_name, is_eco
+    """
+    query = """
+        SELECT 
+            fuel_id,
+            fuel_name,
+            is_eco
+        FROM dim_fuel
+        ORDER BY fuel_id;
+    """
+
+    rows = fetch_all_dict(query)
+
+    # Decimal → int 변환 포함
+    return [
+        {
+            "fuel_id": int(r["fuel_id"]),
+            "fuel_name": r["fuel_name"],
+            "is_eco": r["is_eco"]
+        }
+        for r in rows
+    ]
+
+"""------------------------------------------------"""
+
+from utils.db_utils import fetch_all_dict
+
+def get_sido_list():
+    """
+    D106 - 시도 목록 조회
+    Returns: list(dict) — sido_name
+    """
+    query = """
+        SELECT 
+            sido_name
+        FROM dim_region_sido
+        ORDER BY sido_name;
+    """
+
+    rows = fetch_all_dict(query)
+
+    return [
+        {"sido_name": r["sido_name"]}
+        for r in rows
+    ]
--- backend/test_car_repo.py
+++ backend/test_car_repo.py
@@ -1,13 +1,11 @@
-from db_main.database import get_connection, close_connection
-from db_main.car_repository import get_vehicle_list
-conn = get_connection()
+from db_main.car_repository import get_vehicle_stock_search
 
-try:
+res = get_vehicle_stock_search(
+    year=2025,
+    month=10,
+    sido_id=11,
+    vehicle_kind="승용",
+    limit=20
+)
 
-    res = get_vehicle_list("region", "신규", 2025, 10)
-    print(res)
-
-finally:
-    # 3️⃣ DB / SSH 터널 종료
-    close_connection(conn)
-
+print(res)
