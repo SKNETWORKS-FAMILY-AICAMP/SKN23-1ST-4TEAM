14ec440a5d2b3892b19dfb0a00caeb42270f1b9e
JUYEOP024 <juyeobsong63@gmail.com>
2025-12-12 20:58:24 +0900

    함수정리

--- test_db.py
+++
@@ -1,5 +0,0 @@
-from backend.db_main.database import get_connection, close_connection
-
-conn = get_connection()
-print("connected:", conn is not None)
-close_connection(conn)
--- test_owner.py
+++
@@ -1,4 +0,0 @@
-from backend.db_main.owner_repository import get_inheritance_gift_top3_regions
-
-res = get_inheritance_gift_top3_regions()
-print(res)
